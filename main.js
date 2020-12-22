22695e5f1ac2e70d0aa126126b83e339294f3d01
g939ss <75403348+g939ss@users.noreply.github.com>
2020-12-22 17:42:22 +0900

    Add two files

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = 'その日の気温は華氏18度で外は晴れていた。梅子は:inserty:で:insertz:。:inserty:では、:insertx:が300ポンドの:insertw:いた。梅子はそれを見て驚いた。:insertx:が:insertw:いるのは珍しいことだからだ。';
+let insertX = ['桃太郎','浦島太郎','金太郎'];
+let insertY = ['公園','海辺','山'];
+let insertZ = ['散歩をしていた','ゴミ拾いをしていた','食事をしていた'];
+let insertW = ['鬼と戦って','亀を助けて','熊と戦って'];
 
 randomize.addEventListener('click', result);
 
@@ -20,22 +21,26 @@ function result() {
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
+  let wItem = randomValueFromArray(insertW);
 
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(':insertw:',wItem);
+  newStory = newStory.replace(':insertw:',wItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob',name);
+    newStory = newStory.replace('梅子',name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
+  if(document.getElementById("jp").checked) {
+    const weight = Math.round(300*0.0714286) + ' kg';
+    const temperature =  '摂氏' + Math.round((94-32) * 5 / 9);
+    newStory = newStory.replace('華氏18',temperature);
+    newStory = newStory.replace('300ポンド',weight);
   }
 
   story.textContent = newStory;
